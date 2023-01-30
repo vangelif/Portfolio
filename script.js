a30dd345bcfd8b0a383e72ea8a236c11453184b6
Vangelis <ev.fotiades@yahoo.com>
2023-01-30 15:44:38 +0200

    Include menu appears-disappears functions

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let btn = document.querySelector('#btn-menu');
 
-btn.addEventListener('click', function()) {
-    
-}
+btn.addEventListener('click', function() {
+    let menu = document.querySelector('.btn-menu');
+    menu.style.display = 'flex';
+})
+
+let xBtn = document.querySelector('.fa-xmark');
+
+xBtn.addEventListener('click', function(){
+    let menu= document.querySelector('.btn-menu');
+    menu.style.display = 'none';
+})
