73b31d0468d3a350e0b13bb93b9f7f7b56a3a8c4
Precious Betine <preciousbetine@gmail.com>
2023-01-30 14:55:27 +0100

    Hide mobile menu and display respective page section

--- script.js
+++ script.js
@@ -8,6 +8,25 @@ btn.addEventListener('click', function() {
 let xBtn = document.querySelector('.fa-xmark');
 
 xBtn.addEventListener('click', function(){
-    let menu= document.querySelector('.btn-menu');
-    menu.style.display = 'none';
+  let menu= document.querySelector('.btn-menu');
+  menu.style.display = 'none';
+})
+
+let portfolioLink = document.querySelector('#portfolio-link')
+let aboutLink = document.querySelector('#about-link')
+let contactLink = document.querySelector('#contact-link')
+
+portfolioLink.addEventListener('click', () => {
+  let menu= document.querySelector('.btn-menu');
+  menu.style.display = 'none';
+})
+
+aboutLink.addEventListener('click', () => {
+  let menu= document.querySelector('.btn-menu');
+  menu.style.display = 'none';
+})
+
+contactLink.addEventListener('click', () => {
+  let menu= document.querySelector('.btn-menu');
+  menu.style.display = 'none';
 })
