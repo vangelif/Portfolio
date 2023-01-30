5ade69ef3b4749751a8991f8144ab3852643b7ed
Vangelis <ev.fotiades@yahoo.com>
2023-01-30 16:04:16 +0200

    Fix linter errors

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-let btn = document.querySelector('#btn-menu');
+let btn = document.querySelector("#btn-menu");
 
-btn.addEventListener('click', function() {
-    let menu = document.querySelector('.btn-menu');
-    menu.style.display = 'flex';
-})
+btn.addEventListener("click", function () {
+  let menu = document.querySelector(".btn-menu");
+  menu.style.display = "flex";
+});
 
-let xBtn = document.querySelector('.fa-xmark');
+let xBtn = document.querySelector(".fa-xmark");
 
-xBtn.addEventListener('click', function(){
-  let menu= document.querySelector('.btn-menu');
-  menu.style.display = 'none';
-})
+xBtn.addEventListener("click", function () {
+  let menu = document.querySelector(".btn-menu");
+  menu.style.display = "none";
+});
 
-let portfolioLink = document.querySelector('#portfolio-link')
-let aboutLink = document.querySelector('#about-link')
-let contactLink = document.querySelector('#contact-link')
+let portfolioLink = document.querySelector("#portfolio-link");
+let aboutLink = document.querySelector("#about-link");
+let contactLink = document.querySelector("#contact-link");
 
-portfolioLink.addEventListener('click', () => {
-  let menu= document.querySelector('.btn-menu');
-  menu.style.display = 'none';
-})
+portfolioLink.addEventListener("click", () => {
+  let menu = document.querySelector(".btn-menu");
+  menu.style.display = "none";
+});
 
-aboutLink.addEventListener('click', () => {
-  let menu= document.querySelector('.btn-menu');
-  menu.style.display = 'none';
-})
+aboutLink.addEventListener("click", () => {
+  let menu = document.querySelector(".btn-menu");
+  menu.style.display = "none";
+});
 
-contactLink.addEventListener('click', () => {
-  let menu= document.querySelector('.btn-menu');
-  menu.style.display = 'none';
-})
+contactLink.addEventListener("click", () => {
+  let menu = document.querySelector(".btn-menu");
+  menu.style.display = "none";
+});
